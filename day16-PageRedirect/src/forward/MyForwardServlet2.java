3ca76e4c9e1131ab4dd0f57c4623625f380399d1
GIL SU PARK <prosild@naver.com>
2018-07-25 18:00:14 +0900

    7월 25일 Java 16일차 detail까지 오후 6시까지

---
+++ day16-PageRedirect/src/forward/MyForwardServlet2.java
@@ -0,0 +1,41 @@
+package forward;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 서블릿 페이지 이동 방식 중<br>
+ * Forward 방식을 사용하여 페이지 이동을<br>
+ * 테스트하는 클래스<br>
+ * ------------------------------------------<br>
+ * Forward 방식으로 페이지를 이동시키면<br>
+ * 최초의 request, response가 유지되며 이동한다<br>
+ * 
+ * @author PC38218
+ *
+ */
+@WebServlet("/forward2")
+public class MyForwardServlet2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 1. 첫 서블릿 진입하여 request에 속성 추가
+		request.setAttribute("name", "박길수");
+		
+		// 2. forward 방식으로 next 서블릿으로 이동
+		// (1) RequestDispatcher 객체를 얻어냄
+		RequestDispatcher reqd;
+		reqd = request.getRequestDispatcher("nextJsp");
+		
+		// (2) 얻어진 dispatcher 객체 reqd에 forward() 사용
+		//	   이때, 최초의 request, response를 전달
+		reqd.forward(request, response);
+	}
+
+}
---
+++ day16-PageRedirect/src/redirect/MySendRedirectServlet2.java
@@ -0,0 +1,45 @@
+package redirect;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 서블릿 페이지 이동 방식 중<br>
+ * sendRedirect 방식을 사용하여 페이지 이동을<br>
+ * 테스트하는 클래스<br>
+ * ------------------------------------------<br>
+ * send redirect 기법은<br>
+ * 최초의 request에 대하여 response를 일으키고<br>
+ * 새로운 request로 다음 서블릿을 요청한다<br>
+ * 
+ * 따라서 최초의 request에 설정했던 속성 등이<br>
+ * 유지되지 않는다.<br>
+ * ------------------------------------------<br>
+ * @author PC38218
+ *
+ */
+@WebServlet("/redirect2")
+public class MySendRedirectServlet2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * redirect 목적 페이지인 next로 이동하기 전<br>
+	 * 현재의 request에 대한 응답이 일어나기 때문에<br>
+	 * 브라우저 주소창에 요청 주소가 다음 페이지로 변경됨<br>
+	 * 
+	 * 그리고 request가 변경되기 때문에 설정한 속성이 사라짐
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 1. request 객체에 name 속성을 설정
+		request.setAttribute("name", "박길수");
+		
+		// 2. sendRedirect()로 next를 요청
+		//    redirect는 응답을 일으키므로 response에 적용
+		response.sendRedirect("nextJsp");
+	}
+
+}
